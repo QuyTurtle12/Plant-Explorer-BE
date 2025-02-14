3c9cc36fc01c799df88e8b31f9c1d0ec014c0309
Quy <96563375+QuyTurtle12@users.noreply.github.com>
2025-02-14 18:56:46 +0700

    Added Get All User method

---
+++ Plant-Explorer.Contract.Repositories/ModelViews/UserModel/BaseUserModel.cs
@@ -0,0 +1,14 @@
+﻿using Plant_Explorer.Core.Constants.Enum.EnumUser;
+
+namespace Plant_Explorer.Contract.Repositories.ModelViews.UserModel
+{
+    public class BaseUserModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Role { get; set; } = string.Empty;
+        public int Status { get; set; }
+        public string? AvatarUrl { get; set; }
+    }
+}
