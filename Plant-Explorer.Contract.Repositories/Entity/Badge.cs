2c6f487d0689259ab95a901854f64a3f74412ec5
QuyTurtle12 <Ngocquyquy29@gmail.com>
2025-03-06 08:50:27 +0700

    Add new attribute in badge

--- Plant-Explorer.Contract.Repositories/Entity/Badge.cs
+++ Plant-Explorer.Contract.Repositories/Entity/Badge.cs
@@ -6,6 +6,7 @@ namespace Plant_Explorer.Contract.Repositories.Entity
     {
         public string? Description { get; set; }
         public string? Type { get; set; }
+        public int? conditionalPoint { get; set; }
 
         // Navigation Properties
         public virtual ICollection<UserBadge>? UserBadges { get; set; }
--- Plant-Explorer.Contract.Repositories/ModelViews/BadgeModel/BaseBadgeModel.cs
+++ Plant-Explorer.Contract.Repositories/ModelViews/BadgeModel/BaseBadgeModel.cs
@@ -5,5 +5,6 @@
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string? Type { get; set; }
+        public int? conditionalPoint { get; set; }
     }
 }
